fba2b40eae30346ca879b2bb3520e46708c53d37
Fullstackken <157365920+Fullstackken@users.noreply.github.com>
2024-01-29 01:55:12 -0800

    Add files via upload
    Added new entry earning btn and form input for user to input new earnings into the system.

--- functions.js
+++ functions.js
@@ -3,13 +3,19 @@
 function spendingForm(){
     const spendingFormInputs = `
     <form>
+    
+        <select class="form-select" >
+        <option value="Food" >Food</option>
+        <option value="Transportation" >Transportation</option>
+        <option value="Entertaiment" >Entertaiment</option>
+        </select>
           <label>Location</label>
           <input class="">
         
           <label>Amount</label>
-          <input class="">
+          <input class="" id="spending-amount">
         
-        <button class="btn" onclick="clearSpending()">Submit</button>
+        <button class="btn" onclick="spendingAmountValidated()">Submit</button>
     </form>
     `
 
@@ -21,4 +27,35 @@ function spendingForm(){
 
 function clearSpending(){
     document.getElementById('spending-form').innerHTML = " ";
+}
+
+
+function spendingAmountValidated(){
+    
+    let input_amount = document.getElementById("spending-amount").value;
+
+    console.log("value:", input_amount);
+
+    if(isNaN(input_amount)){
+        console.log("Enter a number");
+    }
+}
+
+function newEarningForm() {
+     const new_earning_form = `
+    <form>
+     <div class="">
+        <label class="form-label">New Earning</label>
+        <input class="form-control">
+    </div> 
+    <div class="new-entry-submit-btn"
+        onclick="clearNewEntryForm()"> Enter </div>
+    </form>
+    `;
+
+    document.getElementById('new-earning-form').innerHTML = new_earning_form;
+}
+
+function clearNewEntryForm() {
+    document.getElementById('new-earning-form').innerHTML = ` `;
 }
